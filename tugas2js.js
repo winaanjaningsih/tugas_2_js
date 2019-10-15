fcd616efabda5a53eecb253339fe6432f7a210e1
winaanjaningsih <zanwina@gmail.com>
2019-10-15 23:00:08 +0700

    tugas  2 js
    mengumpulkan

---
+++ tugas2js.js
@@ -0,0 +1,10 @@
+var usia = 22;
+var nama = "wina anjaningsih";
+var siswa = true;
+var kosong;
+var ganjil = [1,3,5,7];
+console.log(usia)
+console.log(nama)
+console.log(siswa)
+console.log(kosong)
+console.log(ganjil)
